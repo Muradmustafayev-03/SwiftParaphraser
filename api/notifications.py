9b7f6023a00b2eed22c9aae386318580c16846a3
Murad <muradmustafayev03@gmail.com>
2023-09-22 17:32:58 +0400

    exception handling for notification functions

--- api/notifications.py
+++ api/notifications.py
@@ -1,10 +1,17 @@
+import os
+
+
 def notify(project_id, message):
+    os.makedirs('notifications/', exist_ok=True)
     with open(f'notifications/{project_id}.txt', 'w') as file:
         file.write(message)
     print(message)
 
 
 def receive_notification(project_id):
-    with open(f'notifications/{project_id}.txt', 'r') as file:
-        message = file.read()
-    return message
+    try:
+        with open(f'notifications/{project_id}.txt', 'r') as file:
+            message = file.read()
+        return message
+    except FileNotFoundError:
+        return None
