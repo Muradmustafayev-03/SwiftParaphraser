ea35e209c23d156c1eb09a83b52b7fc954e677d5
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2023-10-26 20:05:36 +0400

    add_dummy_files

--- api/scripts/__init__.py
+++ api/scripts/__init__.py
@@ -1,5 +1,5 @@
 from .dict_utils import dir_to_dict, dict_to_dir, apply_to_project
-from .dummy_files import generate_file_content
+from .dummy_files import add_dummy_files
 from .comment_utils import add_comments
 from .rename_utils import *
 from .text import *
