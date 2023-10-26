fdff231e11fc5b7aa1d12b6a4e4b78a7fbedda6f
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2023-10-26 15:13:32 +0400

    init import

--- api/scripts/__init__.py
+++ api/scripts/__init__.py
@@ -1,4 +1,5 @@
 from .dict_utils import dir_to_dict, dict_to_dir, apply_to_project
+from .dummy_files import generate_file_content
 from .comment_utils import add_comments
 from .rename_utils import *
 from .text import *
