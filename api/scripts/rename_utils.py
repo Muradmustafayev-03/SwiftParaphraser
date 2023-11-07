d5a1b952c8e0394c868f13de3267f5b47951562e
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2023-11-07 17:57:12 +0400

    simplified rename

--- api/scripts/rename_utils.py
+++ api/scripts/rename_utils.py
@@ -61,12 +61,7 @@ def new_var_name(name: str):
     name = open_abbreviation(name)
     name = first_letter_upper(name)
 
-    prefix = random.choice(['var', 'variable', 'value'])
-
-    if random.choice([True, False]):
-        return prefix + random.choice(ADJECTIVES) + name
-    else:
-        return generate_random_name(prefix)
+    return 'var' + random.choice(ADJECTIVES) + name
 
 
 def new_type_name(name: str):
@@ -79,10 +74,7 @@ def new_type_name(name: str):
     name = open_abbreviation(name)
     name = first_letter_upper(name)
 
-    if random.choice([True, False]):
-        return 'Type' + random.choice(ADJECTIVES) + name
-    else:
-        return generate_random_name('Type')
+    return 'Type' + random.choice(ADJECTIVES) + name
 
 
 def rename_local_variables(code, function: str):
