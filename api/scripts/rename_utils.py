cd724865a7794df8cc9c872e663d019310876304
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2023-11-11 15:01:56 +0400

    file renaming debug

--- api/scripts/rename_utils.py
+++ api/scripts/rename_utils.py
@@ -243,10 +243,9 @@ def rename_type(project: dict, old_name: str, new_name: str):
 
     for file_path, file_content in project.items():
         if file_path.endswith('.swift'):
-            new_path = file_path
             # pattern if old name is not surrounded by alphanumeric characters
             pattern = rf'(?<![a-zA-Z0-9_]){old_name}(?![a-zA-Z0-9_])'
-            new_project[new_path] = re.sub(pattern, new_name, file_content)
+            new_project[file_path] = re.sub(pattern, new_name, file_content)
             continue
         elif file_path.endswith('.xib') or file_path.endswith('.storyboard'):
             pattern = r'customClass="' + re.escape(old_name) + r'"'
@@ -303,8 +302,8 @@ def rename_files(project: dict, rename_map: dict) -> dict:
             continue
 
         for old_name, new_name in rename_map.items():
-            new_path = new_path.replace(old_name + '.swift', new_name + '.swift')
-            new_path = new_path.replace(old_name + '.xib', new_name + '.xib')
+            new_path = new_path.replace('/' + old_name + '.swift', '/' + new_name + '.swift')
+            new_path = new_path.replace('/' + old_name + '.xib', '/' + new_name + '.xib')
         new_project[new_path] = file_content
 
     return new_project
