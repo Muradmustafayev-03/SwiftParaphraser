fef8d7fd150b633ff6d5d4aa0373a9e18fa939b0
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2023-11-18 16:13:21 +0400

    created apply_to_files function

--- api/scripts/file_utils.py
+++ api/scripts/file_utils.py
@@ -80,3 +80,18 @@ def project_contains_string(project: dict, string: str) -> bool:
         if string in file_content:
             return True
     return False
+
+
+def apply_to_files(dir_path: str, func: callable, exclude=(), file_types: tuple = '.swift', *args, **kwargs):
+    for root, dirs, files in os.walk(dir_path):
+        # Check if any folder in the path is in FRAMEWORKS
+        if 'Pods' in root.replace('\\', '/').split('/'):
+            continue  # skip files in frameworks
+        for file in files:
+            for file_type in file_types:
+                if file.endswith(file_type) and not file.startswith('._') and file not in exclude:
+                    with open(file, 'r', encoding='utf-8') as f:
+                        content = f.read().replace('\u2028', ' ')
+                        new_content = func(content, *args, **kwargs)
+                    with open(file, 'w', encoding='utf-8') as f:
+                        f.write(new_content)
