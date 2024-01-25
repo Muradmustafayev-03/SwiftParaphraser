bfbb47c5f77b6df5381564cb18689375f9c9f2a7
Muradmustafayev-03 <muradmustafayev03@gmail.com>
2024-01-25 15:24:50 +0400

    pipeline optimized and rename maps managing problem fixed

--- api/pipeline.py
+++ api/pipeline.py
@@ -37,6 +37,7 @@ def pipeline(unique_id: str, path: str,
     :param function_transformation: bool, whether to restructure functions, unstable, recommended being True
     :param variable_renaming: bool, whether to rename variables, stable, recommended being True
     :param comment_adding: bool, whether to add comments, stable, recommended being True (takes a long time)
+    :param dummy_file_adding: bool, whether to add dummy files, stable, recommended being True
     """
     preprocess(unique_id, path)
     notify(unique_id, 'Finished preprocessing the project...')
@@ -66,33 +67,46 @@ def pipeline(unique_id: str, path: str,
         apply_to_files(path, add_comments)
         notify(unique_id, 'Finished adding comments.')
 
-    project = dir_to_dict(path)
+    if type_renaming or file_renaming or dummy_file_adding:
+        project = dir_to_dict(path)
 
-    type_names = parse_types_in_project(project, include_types=types_to_rename)
-    types_in_frameworks = parse_types_in_frameworks(path)
-    type_names = list(set(type_names) - set(types_in_frameworks))
-    file_names = list_file_names(project)
-    all_names = set(type_names + file_names)
-    if 'Package' in all_names:
-        all_names.remove('Package')
-    all_names = list(all_names)
+        type_names = parse_types_in_project(project, include_types=types_to_rename)
+        types_in_frameworks = parse_types_in_frameworks(path)
+        type_names = set(type_names) - set(types_in_frameworks)
+        file_names = set(list_file_names(project))
 
-    if type_renaming and type_names:
-        assert_notify(unique_id, 'Renaming types...')
-        project = rename_types(project, generate_rename_map(all_names))
-        notify(unique_id, 'Finished renaming types.')
+        if 'Package' in file_names:
+            file_names.remove('Package')
 
-    if file_renaming and file_names:
-        assert_notify(unique_id, 'Renaming files...')
-        project = rename_files(project, generate_rename_map(all_names))
-        notify(unique_id, 'Finished renaming files.')
+        common_names = type_names & file_names
+        type_only_names = type_names - common_names
+        file_only_names = file_names - common_names
 
-    if dummy_file_adding:
-        assert_notify(unique_id, 'Adding dummy files...')
-        project = add_dummy_files(project, path)
-        notify(unique_id, 'Finished adding dummy files.')
+        common_rename_map = generate_rename_map(list(common_names))
+        type_rename_map = generate_rename_map(list(type_only_names))
+        file_rename_map = generate_rename_map(list(file_only_names))
 
-    notify(unique_id, 'Finished paraphrasing the project.')
+        type_rename_map.update(common_rename_map)
+        file_rename_map.update(common_rename_map)
 
-    assert_notify(unique_id, 'Saving paraphrased project...')
-    dict_to_dir(project)
+        if type_renaming and type_names:
+            assert_notify(unique_id, 'Renaming types...')
+            project = rename_types(project, type_rename_map)
+            notify(unique_id, 'Finished renaming types.')
+
+        if file_renaming and file_names:
+            assert_notify(unique_id, 'Renaming files...')
+            project = rename_files(project, file_rename_map)
+            notify(unique_id, 'Finished renaming files.')
+
+        if dummy_file_adding:
+            assert_notify(unique_id, 'Adding dummy files...')
+            project = add_dummy_files(project, path)
+            notify(unique_id, 'Finished adding dummy files.')
+
+        notify(unique_id, 'Finished paraphrasing the project.')
+        assert_notify(unique_id, 'Saving paraphrased project...')
+        dict_to_dir(project)
+
+    else:
+        notify(unique_id, 'Finished paraphrasing the project. The project is already saved.')
